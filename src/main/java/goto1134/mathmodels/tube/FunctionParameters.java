ca2041092ae27360b34db0ad7edc2599bdb5c492
Andrew Yefanov <1134togo@gmail.com>
2016-12-18 14:10:13 +0300

    now FunctionParameters have package-private getters

--- src/main/java/goto1134/mathmodels/tube/FunctionParameters.java
+++ src/main/java/goto1134/mathmodels/tube/FunctionParameters.java
@@ -1,25 +1,21 @@
 package goto1134.mathmodels.tube;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 
 /**
  * Created by Andrew
  * on 18.12.2016.
  */
-@Getter
+@Getter(AccessLevel.PACKAGE)
 class FunctionParameters {
-    @Getter
     private final double a;
-    @Getter
     private final double b;
-    @Getter
     private final double c;
-    @Getter
     private final double d;
-    @Getter
     private final double e;
 
-    public FunctionParameters(double a, double b, double c, double d, double e) {
+    FunctionParameters(double a, double b, double c, double d, double e) {
         this.a = a;
         this.b = b;
         this.c = c;
